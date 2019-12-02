42dda9a2e6b05f1b55c52a4164b68d2655c4181d
edevars <e.devars.dev@gmail.com>
2019-12-02 03:22:57 -0600

    :sparkles: Deleting items from the menu

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -31,36 +31,14 @@ const Layout = (props) => {
                 </nav>
 
                 <nav className="navMenu"> 
-                    <Link to="/Destinos">
+                    <Link to="/destinos">
                     <button className="opcButton" onClick={handleClick}>
                         Destinos
                     </button>
                     </Link>
                 </nav>
 
-                <nav className="navMenu"> 
-                    <Link to="/">
-                    <button className="opcButton" onClick={handleClick}>
-                        + Visitados
-                    </button>
-                    </Link>
-                </nav>
-
-                <nav className="navMenu"> 
-                    <Link to="/Reservaciones">
-                    <button className="opcButton" onClick={handleClick}>
-                        Hacer una Reservación
-                    </button>
-                    </Link>
-                </nav>
-
-                <nav className="navMenu"> 
-                    <Link to="/Agenda">
-                    <button className="opcButton" onClick={handleClick}>
-                        Tu Agenda
-                    </button>
-                    </Link>
-                </nav>
+                
             </div>
             <div className="content">
                 {props.children}
